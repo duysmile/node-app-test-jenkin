b46612916c4b6bacb236fa581b949204261d7394
DuyN <duy210697@gmail.com>
2019-09-11 14:08:45 +0700

    update view

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
+ request(app).get('/').expect('hello world yeah', done);
  });
 });
