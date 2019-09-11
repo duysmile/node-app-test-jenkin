8a70b5287ee4c5e973313b5eed324fefa166ca96
DuyN <duy210697@gmail.com>
2019-09-11 14:53:28 +0700

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-ssh -tt duy210697@35.237.235.110 <<EOF
+ssh /root/.ssh/id_rsa -tt duy210697@35.237.235.110 <<EOF
  sudo -s
  cd /home/duy210697/node-app-test-jenkin/
  git pull
  npm install
  pm2 restart all
+ exit
  exit
 EOF
